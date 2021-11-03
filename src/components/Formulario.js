97caf59ba806c1a9ff1be2b19d4ae9b118c637cb
Jonathan Pinto <jonathanpintopp@gmail.com>
2021-11-03 08:05:01 -0500

    feat: Form feature added

---
+++ src/components/Formulario.js
@@ -0,0 +1,45 @@
+import React from "react";
+
+const Formulario = () => {
+  return (
+    <div className="bg-info">
+      <div className="container">
+        <div className="row">
+          <form className="col card text-white bg-transparent mb-5 pt-5 pb-2">
+            <fieldset>
+              <legend className="text-center">Buscador Letras Canciones</legend>
+
+              <div className="row">
+                <div className="col-md-6">
+                  <div className="form-group">
+                    <label>Artista</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      name="artista"
+                      placeholder="Nombre Artista"
+                    />
+                  </div>
+                </div>
+
+                <div className="col-md-6">
+                  <div className="form-group">
+                    <label>Canción</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      name="cancion"
+                      placeholder="Nombre Canción"
+                    />
+                  </div>
+                </div>
+              </div>
+            </fieldset>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Formulario;
