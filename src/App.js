f5bb11b97cb10fe95ff47378447ebbfd181dffa7
Jonathan Pinto <jonathanpintopp@gmail.com>
2021-11-03 13:19:30 -0500

    feat: refactor  added

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import React, { Fragment, useState, useEffect } from "react";
 
 import Formulario from "./components/Formulario";
 import Cancion from "./components/Cancion";
+import Info from "./components/Info";
 
 import axios from "axios";
 
 function App() {
   // definir el state
   const [busquedaletra, guardarBusquedaLetra] = useState({});
-  const [letra, guardaLetra] = useState("");
+  const [letra, guardarLetra] = useState("");
   const [info, guardarInfo] = useState({});
-
 
   useEffect(() => {
     if (Object.keys(busquedaletra).length === 0) return;
@@ -19,28 +19,16 @@ function App() {
       const { artista, cancion } = busquedaletra;
 
       const url = `https://api.lyrics.ovh/v1/${artista}/${cancion}`;
-  const url2= `theaudiodb.com/api/v1/json/1/search.php?s=coldplay
-  `
+      const url2 = `https://www.theaudiodb.com/api/v1/json/1/search.php?s=${artista}
+  `;
 
+      const [letra, informacion] = await Promise.all([axios(url), axios(url2)]);
 
-   const [letra, informacion] = await Promise.all([
-     axios(url),
-     axios(url2)
-   ]);
-
-   
-
-   guardarLetra(letra.data.lyrics);
-   guardarInfo(informacion.data.artists[0]);
- 
-
-
-
-
-     
+      guardarLetra(letra.data.lyrics);
+      guardarInfo(informacion.data.artists[0]);
     };
     consultarApiLetra();
-  }, [busquedaletra,info]);
+  }, [busquedaletra, info]);
 
   return (
     <Fragment>
@@ -48,16 +36,12 @@ function App() {
 
       <div className="container mt-5">
         <div className="row">
-          <div className="col-md-6"></div>
+          <div className="col-md-6">
+            <Info info={info} />
+          </div>
 
           <div className="col-md-6">
-
-            <Cancion
-            
-            letra={letra}
-            
-            
-            />
+            <Cancion letra={letra} />
           </div>
         </div>
       </div>
