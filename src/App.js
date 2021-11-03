8ce859a3704dea97225d26fdcbd54c4151a781f1
Jonathan Pinto <jonathanpintopp@gmail.com>
2021-11-03 10:30:26 -0500

    feat: showing lyrics

--- src/App.js
+++ src/App.js
@@ -1,41 +1,49 @@
 import React, { Fragment, useState, useEffect } from "react";
 
 import Formulario from "./components/Formulario";
+import Cancion from "./components/Cancion";
 
-
-import axios from 'axios';
+import axios from "axios";
 
 function App() {
   // definir el state
   const [busquedaletra, guardarBusquedaLetra] = useState({});
-  const [letra,guardaLetra] = useState('');
-
-
-
+  const [letra, guardaLetra] = useState("");
 
   useEffect(() => {
     if (Object.keys(busquedaletra).length === 0) return;
 
-
     const consultarApiLetra = async () => {
+      const { artista, cancion } = busquedaletra;
 
-      const {artista, cancion} = busquedaletra;
+      const url = `https://api.lyrics.ovh/v1/${artista}/${cancion}`;
 
+      const resultado = await axios(url);
 
-     const url = `https://api.lyrics.ovh/v1/${artista}/${cancion}`
-
-
-     const resultado =  await axios (url);
-
-    guardaLetra(resultado.data.lyrics);
-    }
-consultarApiLetra();
-
+      guardaLetra(resultado.data.lyrics);
+    };
+    consultarApiLetra();
   }, [busquedaletra]);
 
   return (
     <Fragment>
       <Formulario guardarBusquedaLetra={guardarBusquedaLetra} />
+
+      <div className="container mt-5">
+        <div className="row">
+          <div className="col-md-6"></div>
+
+          <div className="col-md-6">
+
+            <Cancion
+            
+            letra={letra}
+            
+            
+            />
+          </div>
+        </div>
+      </div>
     </Fragment>
   );
 }
